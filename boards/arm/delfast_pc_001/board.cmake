d82e47ad3aba25f21aa2f0854587fb9ffc1ac256
Denys Batrak <baden.i.ua@gmail.com>
2021-10-14 03:18:47 +0300

    test local openocd

--- boards/arm/delfast_pc_001/board.cmake
+++ boards/arm/delfast_pc_001/board.cmake
@@ -2,5 +2,8 @@
 
 board_runner_args(nulink "-f")
 
+set(OPENOCD "~/.local/openocd/bin/openocd" CACHE FILEPATH "" FORCE)
+set(OPENOCD_DEFAULT_PATH ~/.local/openocd/share/openocd/scripts)
+
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nulink.board.cmake)

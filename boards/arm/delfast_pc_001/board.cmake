f68f87cbfc84ca96ba25d29dec3a1e9d7cafd1f4
Denys Batrak <baden.i.ua@gmail.com>
2021-11-14 20:05:30 +0200

    Move boart definition from library to project.

--- boards/arm/delfast_pc_001/board.cmake
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-board_runner_args(nulink "-f")
-
-set(OPENOCD "~/.local/openocd/bin/openocd" CACHE FILEPATH "" FORCE)
-set(OPENOCD_DEFAULT_PATH "~/.local/openocd/share/openocd/scripts" CACHE FILEPATH "" FORCE)
-
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/nulink.board.cmake)
